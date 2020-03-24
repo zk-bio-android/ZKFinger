1e8838be207c6a447b2c264ece9202d027d86867
xiaoqiang <1341974376@qq.com>
2020-03-24 17:54:31 +0800

    1.create project.

---
+++ ZKFingerSDKdemo/app/src/main/java/com/zkteco/zkfingersdkdemo/MainActivity.java
@@ -0,0 +1,19 @@
+package com.zkteco.zkfingersdkdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * @author Administrator
+ * Created by：Administrator on 2020/3/16 16:33
+ * Email：qiang.xiao@zkteco.com
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
